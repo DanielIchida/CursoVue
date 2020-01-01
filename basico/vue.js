69baccffa7a95c92bab42cd8756b4f63638fa3ae
Daniel <danielchia76@hotmai.com>
2020-01-01 10:57:06 -0500

    Initial commit

---
+++ basico/vue.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: ".vue",
+    data: {
+        mensaje: "Daniel",
+        imagen: "https://vuejs.org/images/logo.png"
+    },
+    methods: {
+        mostrarMensaje: function(){
+            return "Aprender"
+        }
+    }
+});
