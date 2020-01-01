fa4ee1c62a781fca03523a80e0d1b3e66514abef
Daniel <danielchia76@hotmai.com>
2020-01-01 11:34:11 -0500

    version 2

--- basico/vue.js
+++ basico/vue.js
@@ -2,11 +2,43 @@ var app = new Vue({
     el: ".vue",
     data: {
         mensaje: "Daniel",
-        imagen: "https://vuejs.org/images/logo.png"
+        imagen: "https://vuejs.org/images/logo.png",
+        primero: 0,
+        segundo: 0,
+        nombre: ''
     },
     methods: {
         mostrarMensaje: function(){
-            return "Aprender"
+            return "Aprender";
         }
+    },
+    computed: {
+        total: function(){
+            return this.primero + this.segundo;
+        }
+    },
+    beforeCreate: function(){
+        console.log("beforeCreate");
+    },
+    created: function(){
+        console.log("created");
+    },
+    beforeMount: function(){
+        console.log("beforeMount");
+    },
+    mounted: function(){
+        console.log("mounted");
+    },
+    beforeUpdate: function(){
+        console.log("beforeUpdate");
+    },
+    updated: function(){
+        console.log("updated");
+    },
+    beforeDestroy: function(){
+        console.log("beforeDestroy");
+    },
+    destroyed: function(){
+        console.log("destroyed");
     }
 });
