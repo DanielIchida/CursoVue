d5d461c7ad06e66eb6caced3420e0d3c69d9796b
Daniel <danielchia76@hotmai.com>
2020-02-18 21:32:41 -0500

    Rutas

--- miscomponentes/datatable/index.js
+++ miscomponentes/datatable/index.js
@@ -3,10 +3,17 @@ Vue.component('data-table',{
         titles: Array,
         datalist: Array,
         button: Array,
+        id: String
+    },
+    data: function(){
+        return {
+            content: '',
+            datable: null
+        }
     },
     template: `
      <div class="table-responsive">
-            <table class="table display table-bordered nowrap" id="table-person" cellspacing="0" width="100%">
+            <table class="table table-bordered display nowrap" :id="id" cellspacing="0" width="100%">
                 <thead>
                     <tr>
                         <th># Acciones</th>
@@ -24,21 +31,146 @@ Vue.component('data-table',{
             </table>
       </div>
     `,
+    beforeCreate: function(){
+        console.log("before created");
+    },
+    created: function(){
+       console.log("created");
+        console.log(" - - - ");
+    },
+    beforeMount: function(){
+
+    },
     mounted: function(){
-        $('#table-person').DataTable();
+        this.datable = $("#"+this.id).DataTable();
+    },
+    beforeUpdate: function(){
+        console.log("before update");
+        if(this.datable){
+            this.datable.destroy();
+        }
+    },
+    updated: function(){
+        console.log("updated")
+        if(this.datable){
+            this.datable = $("#"+this.id).DataTable();
+        }
+    },
+    beforeDestroy: function(){
+        console.log("before destroy");
+    },
+    destroyed: function(){
+        this.data = [];
+        console.log("destroyed");
     },
     methods: {
         click: function(metodo,value){
            this.$emit(metodo,value);
-        }
+        },
     }
 });
 new Vue({
    el: "#app",
    data: {
+       datatable: null,
+       identificador: "tabla1",
        titulos: ["Pais","Provincia","Ciudad"],
        data:[
            {
+               "id" : 1,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Madrid"
+           },
+           {
+               "id" : 2,
+               "pais" :  "Colombia",
+               "provincia" : "Quindio",
+               "ciudad" : "Armenia"
+           },
+           {
+               "id" : 3,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Mosquera"
+           },
+           {
+               "id" : 4,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Villeta"
+           },
+           {
+               "id" : 1,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Madrid"
+           },
+           {
+               "id" : 2,
+               "pais" :  "Colombia",
+               "provincia" : "Quindio",
+               "ciudad" : "Armenia"
+           },
+           {
+               "id" : 3,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Mosquera"
+           },
+           {
+               "id" : 4,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Villeta"
+           }, {
+               "id" : 1,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Madrid"
+           },
+           {
+               "id" : 2,
+               "pais" :  "Colombia",
+               "provincia" : "Quindio",
+               "ciudad" : "Armenia"
+           },
+           {
+               "id" : 3,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Mosquera"
+           },
+           {
+               "id" : 4,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Villeta"
+           },
+           {
+               "id" : 1,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Madrid"
+           },
+           {
+               "id" : 2,
+               "pais" :  "Colombia",
+               "provincia" : "Quindio",
+               "ciudad" : "Armenia"
+           },
+           {
+               "id" : 3,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Mosquera"
+           },
+           {
+               "id" : 4,
+               "pais" :  "Colombia",
+               "provincia" : "Cundinamarca",
+               "ciudad" : "Villeta"
+           }, {
                "id" : 1,
                "pais" :  "Colombia",
                "provincia" : "Cundinamarca",
@@ -74,7 +206,8 @@ new Vue({
                "clases" : "btn btn-warning",
                "metodo" : "update"
            }
-       ]
+       ],
+       tabla: ""
    },
     methods: {
         borrar: function(value){
@@ -82,7 +215,41 @@ new Vue({
         },
         actualizar: function(value){
 
+        },
+        beforeUpdate: function(){
+            this.identificador = "tabla1";
+        },
+        cambiarData: function(){
+            this.data = [];
+            this.data = [
+                {
+                    "id" : 5,
+                    "pais" :  "Colombia",
+                    "provincia" : "Cundinamarca",
+                    "ciudad" : "Facatativa"
+                },
+                {
+                    "id" : 6,
+                    "pais" :  "Colombia",
+                    "provincia" : "Boyaca",
+                    "ciudad" : "Tunja"
+                },
+                {
+                    "id" : 7,
+                    "pais" :  "Colombia",
+                    "provincia" : "Cundinamarca",
+                    "ciudad" : "Funza"
+                },
+                {
+                    "id" : 8,
+                    "pais" :  "Colombia",
+                    "provincia" : "Cundinamarca",
+                    "ciudad" : "Guaduas"
+                }
+            ];
+            this.identificador = "tabla2";
         }
-    }
+    },
+
 });
 
